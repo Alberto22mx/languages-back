0e4e4bf569de7c9fa31af7c6341d1d646d175cb0
Jose Alberto <jose.serrano@blautech.com>
2025-01-18 21:23:37 -0600

    Se edita create groups

--- src/groups/dto/create-groups.dto.ts
+++ src/groups/dto/create-groups.dto.ts
@@ -1,8 +1,3 @@
-import { Exams } from 'src/exam/schemas/exams.schema';
-import { Games } from 'src/games/schemas/games.schema';
-import { Lessons } from 'src/lessons/lessons.entity';
-import { User } from 'src/users/schemas/user.schema';
-
 export class CreateGroupsDto {
   id: string;
   nameGroup: string;
@@ -10,8 +5,8 @@ export class CreateGroupsDto {
   schedule: string;
   state: string;
   image: string;
-  exams: Exams[];
-  lessons: Lessons[];
-  games: Games[];
-  users: User[];
+  exams: string[];
+  lessons: string[];
+  games: string[];
+  users: string[];
 }
